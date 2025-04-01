70a824875960027eee385d07916920c294b7b20b
AxelWin4566 <axelwinants223@gmail.com>
2025-04-01 20:23:57 +0200

    axouu

---
+++ res/access.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = '1mlspiup';
+$imSettings['access']['users'] = array(
+	'axelwinants223@gmail.com' => array(
+		'groups' => array('q5v0xin8'),
+		'id' => 'bwm40gip',
+		'firstname' => 'Admin',
+		'lastname' => '',
+		'password' => 'sivodk7f',
+		'email' => 'axelwinants223@gmail.com',
+		'page' => false
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('bwm40gip');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
---
+++ res/x5settings.php
@@ -0,0 +1,111 @@
+<?php
+
+/*
+|-------------------------------
+|	GENERAL SETTINGS
+|-------------------------------
+*/
+
+$imSettings['general'] = array(
+	'url' => 'http://www.axel.com/',
+	'homepage_url' => 'http://www.axel.com/index.html',
+	'icon' => '',
+	'version' => '14.0.4.1',
+	'sitename' => 'Rencontre',
+	'public_folder' => '',
+	'salt' => 'gdk3anivomq72kxq2ai8djn4asmcw8sr61gn55hnqxhwinhrgnkw0b',
+	'use_common_email_sender_address' => false,
+	'common_email_sender_addres' => ''
+);
+
+
+$imSettings['admin'] = array(
+	'notification_public_key' => '73cd765c59e05d7b',
+	'notification_private_key' => '902349d4b019fd97',
+	'enable_manager_notifications' => false,
+	'theme' => 'orange'
+);
+
+
+/*
+|--------------------------------------------------------------------------------------
+|	DATABASES SETTINGS
+|--------------------------------------------------------------------------------------
+*/
+
+$imSettings['databases'] = array();
+$ecommerce = Configuration::getCart();
+// Setup the coupon data
+$couponData = array();
+$couponData['products'] = array();
+// Setup the cart
+$ecommerce->setPublicFolder('');
+$ecommerce->setCouponData($couponData);
+$ecommerce->setSettings(array(
+	'force_sender' => false,
+	'email_opening' => 'Cher/chère client(e),<br /><br />Nous vous remercions de votre achat. Nous vous transmettons un récapitulatif de votre commande<br /><br />Vous trouverez ci-dessous la liste des produits commandés, les informations de facturation et les instructions concernant la livraison et le paiement choisi.',
+	'email_closing' => 'Nous contacter pour obtenir de plus amples informations.<br /><br />Nos meilleures salutations, Service commercial.',
+	'email_digital_shipment_opening' => 'Bonjour,<br /><br />Nous vous remercions pour votre achat et nous avons le plaisir de vous envoyer la liste des liens de téléchargement correspondant aux produits commandés :<br />',
+	'email_digital_shipment_closing' => 'Nous restons à votre disposition pour toute information supplémentaire.<br /><br />Cordialement,<br />l’Équipe Commerciale<br />',
+	'email_physical_shipment_opening' => 'Bonjour,<br /><br />Nous vous remercions pour votre achat et nous avons le plaisir de vous envoyer la liste des produits envoyés :',
+	'email_physical_shipment_closing' => 'Nous restons à votre disposition pour toute information supplémentaire.<br /><br />Cordialement,<br />l’Équipe Commerciale',
+	'useCSV' => false,
+	'header_bg_color' => 'rgba(37, 58, 88, 1)',
+	'header_text_color' => 'rgba(255, 255, 255, 1)',
+	'cell_bg_color' => 'rgba(255, 255, 255, 1)',
+	'cell_text_color' => 'rgba(0, 0, 0, 1)',
+	'availability_reduction_type' => 1,
+	'border_color' => 'rgba(211, 211, 211, 1)',
+	'owner_email' => '',
+	'vat_type' => 'included'
+));
+
+$ecommerce->setDigitalProductsData(array());
+
+/*
+|-------------------------------------------------------------------------------------------
+|	GUESTBOOK SETTINGS
+|-------------------------------------------------------------------------------------------
+*/
+
+$imSettings['guestbooks'] = array();
+/*
+|-------------------------------------------------------------------------------------------
+|	Dynamic Objects SETTINGS
+|-------------------------------------------------------------------------------------------
+*/
+
+$imSettings['dynamicobjects'] = array(
+	'template' => array(
+),
+	'pages' => array(
+
+	)
+);
+
+/*
+|-------------------------------
+|	EMAIL SETTINGS
+|-------------------------------
+*/
+
+$ImMailer->emailType = 'phpmailer';
+$ImMailer->exposeWsx5 = true;
+$ImMailer->header = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">' . "\n" . '<html>' . "\n" . '<head>' . "\n" . '<meta http-equiv="content-type" content="text/html; charset=utf-8">' . "\n" . '<meta name="generator" content="Incomedia WebSite X5 v14 - www.websitex5.com">' . "\n" . '</head>' . "\n" . '<body bgcolor="#37474F" style="background-color: #37474F;">' . "\n\t" . '<table border="0" cellpadding="0" align="center" cellspacing="0" style="padding: 0; margin: 0 auto; width: 700px;">' . "\n\t" . '<tr><td id="imEmailContent" style="min-height: 300px; padding: 10px; font: normal normal normal 9pt \'Tahoma\'; color: #000000; background-color: #FFFFFF; text-align: left; text-decoration: none;  width: 700px;border-style: solid; border-color: #000000; border-top-width: 1px; border-right-width: 1px; border-bottom-width: 1px; border-left-width: 1px;background-color: #FFFFFF" width="700px">' . "\n\t\t";
+$ImMailer->footer = "\n\t" . '</td></tr>' . "\n\t" . '</table>' . "\n" . '<table width="100%"><tr><td id="imEmailFooter" style="font: normal normal normal 7pt \'Tahoma\'; color: #FFFFFF; background-color: transparent; text-align: center; text-decoration: none;  padding: 10px; margin-top: 5px;background-color: transparent">' . "\n\t\t" . 'Ce courriel fournit des informations destinées uniquement au destinataire susmentionné.<br>Si vous n\'êtes par le destinataire de ce message, veuillez le signaler immédiatement à l\'expéditeur et le détruire, sans le copier.' . "\n\t" . '</td></tr></table>' . "\n\t" . '</body>' . "\n" . '</html>';
+$ImMailer->bodyBackground = '#FFFFFF';
+$ImMailer->bodyBackgroundEven = '#FFFFFF';
+$ImMailer->bodyBackgroundOdd = '#F0F0F0';
+$ImMailer->bodyBackgroundBorder = '#CDCDCD';
+$ImMailer->bodyTextColorOdd = '#FFFFFF';
+$ImMailer->bodySeparatorBorderColor = '#000000';
+$ImMailer->emailBackground = '#37474F';
+$ImMailer->emailContentStyle = 'font: normal normal normal 9pt \'Tahoma\'; color: #000000; background-color: #FFFFFF; text-align: left; text-decoration: none; ';
+$ImMailer->emailContentFontFamily = 'font-family: Tahoma;';
+ImTopic::$captcha_code = "		<div class=\"x5captcha-wrap\">
+			<label>Code de sécurité :</label><br />
+			<input type=\"text\" class=\"imCpt\" name=\"imCpt\" maxlength=\"5\" />
+		</div>
+";
+
+// End of file x5settings.php
